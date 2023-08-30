a11c7afd6e1b7edbfccc5b8faf5ced5375aa193e
Kevan Hollbach <khollbach@gmail.com>
2023-08-29 22:22:54 -0700

    Add a test program for the TCP code
    It doesn't yet call the reliable versions of send/recv, but it works
    with the normal ones at least.
    
    Next step is to make this call the TCP code instead and see what breaks.

--- tests/lib/tcp-test.c
+++ tests/lib/tcp-test.c
@@ -4,11 +4,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "../../lib/tcp.h"
 #include "../../lib/util.h"
+#include "../../lib/tcp.h"
 
-void ping_random_peer(json_object* original_msg, const char** peers,
-                      size_t num_peers);
+void ping_random_peer(json_object* original_msg, const char** peers, size_t num_peers);
 void pong(json_object* ping);
 void echo(json_object* echo_request);
 
@@ -17,30 +16,25 @@ int main()
     json_object* init_msg = msg_recv();
     if (init_msg == NULL)
     {
-        fprintf(stderr, "Error: expected init message, got EOF\n");
+        fprintf(stderr, "expected init message, got EOF\n");
         exit(EXIT_FAILURE);
     }
 
     const char** peers = node_ids(init_msg);
     size_t num_peers = node_ids_count(init_msg);
-    tcp_init(peers, num_peers);
+    // tcp_init(peers, num_peers);
 
     msg_send(generic_reply(init_msg));
 
     json_object* msg;
-    while ((msg = msg_recv_listener()) != NULL)
+    while ((msg = msg_recv()) != NULL)
     {
         const char* type = msg_type(msg);
-        if (strcmp(type, "echo") == 0)
-        {
+        if (strcmp(type, "echo") == 0) {
             ping_random_peer(msg, peers, num_peers);
-        }
-        else if (strcmp(type, "ping") == 0)
-        {
+        } else if (strcmp(type, "ping") == 0) {
             pong(msg);
-        }
-        else if (strcmp(type, "pong") == 0)
-        {
+        } else if (strcmp(type, "pong") == 0) {
             // Extract the original message
             json_object* body = json_object_object_get(msg, "body");
             json_object* orig = json_object_object_get(body, "original_msg");
@@ -49,22 +43,18 @@ int main()
 
             // Reply to the original client.
             echo(orig);
-        }
-        else
-        {
-            fprintf(stderr, "Error: invalid incoming message type: %s\n", type);
+        } else {
+            fprintf(stderr, "invalid incoming message type: %s\n", type);
             exit(EXIT_FAILURE);
         }
     }
 
-    tcp_free();
+    // tcp_free(peers, num_peers);
     json_object_put(init_msg);
 }
 
 // Takes ownership of original_msg. Borrows `peers`.
-void ping_random_peer(json_object* original_msg, const char** peers,
-                      size_t num_peers)
-{
+void ping_random_peer(json_object* original_msg, const char** peers, size_t num_peers) {
     json_object* ping = generic_reply(original_msg);
 
     // Choose a random peer (possibly myself!)
@@ -80,12 +70,11 @@ void ping_random_peer(json_object* original_msg, const char** peers,
     // Include the original message as a payload.
     json_object_object_add(body, "original_msg", original_msg);
 
-    msg_send_pusher(ping);
+    msg_send(ping);
 }
 
 // Takes ownership of `ping`.
-void pong(json_object* ping)
-{
+void pong(json_object* ping) {
     // Create a "pong" message.
     json_object* pong = generic_reply(ping);
     json_object* pong_body = json_object_object_get(pong, "body");
@@ -94,13 +83,12 @@ void pong(json_object* ping)
 
     // Clone the payload.
     json_object* ping_body = json_object_object_get(ping, "body");
-    json_object* original_msg =
-        json_object_object_get(ping_body, "original_msg");
+    json_object* original_msg = json_object_object_get(ping_body, "original_msg");
     json_object_get(original_msg);
     json_object_put(ping);
     json_object_object_add(pong_body, "original_msg", original_msg);
 
-    msg_send_pusher(pong);
+    msg_send(pong);
 }
 
 // Takes ownership of `echo_request`.
