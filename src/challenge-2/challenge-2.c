57181e8b1831147f14fb630e62323a2c84237b8a
Ravi Dayabhai <r.i.dayabhai@gmail.com>
2023-11-04 20:54:38 -0700

    Finish Challenge 2

--- src/challenge-2/challenge-2.c
+++ src/challenge-2/challenge-2.c
@@ -1,9 +1,12 @@
 #include "../../lib/util.h"
-#include "json-c/json_object.h"
-#include <json-c/json.h>
+#include <limits.h>
 #include <stdio.h>
+#include <string.h>
 
-void queue_json_object_free(void* obj);
+#define MAX_SIGNED_INT64_STRLEN 20
+
+json_object* uniq_id_reply(json_object* uniq_id_msg, const char* node_id,
+                           int64_t* logical_clock);
 
 int main(void)
 {
@@ -14,6 +17,41 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
+    // Node ID + logical timestamp gives unique ids
+    const char* name = node_id(init_msg);
+    char node_id[strlen(name) + 1];
+    strcpy(node_id, name);
+    int64_t logical_clock = 0;
+
     msg_send(generic_reply(init_msg));
     json_object_put(init_msg);
+
+    json_object* uniq_id_msg;
+    while ((uniq_id_msg = msg_recv()) != NULL)
+    {
+        msg_send(uniq_id_reply(uniq_id_msg, node_id, &logical_clock));
+        json_object_put(uniq_id_msg);
+    }
+}
+
+// Borrows `uniq_id_msg`.
+// Returns an owned object.
+json_object* uniq_id_reply(json_object* uniq_id_msg, const char* node_id,
+                           int64_t* logical_clock)
+{
+    json_object* reply = generic_reply(uniq_id_msg);
+
+    json_object* reply_uniq_id;
+
+    // Construct the unique id (node ID + logical timestamp)
+    char logical_timestamp[MAX_SIGNED_INT64_STRLEN + 1];
+    sprintf(logical_timestamp, "%ld", (*logical_clock)++);
+    char uniq_id[strlen(node_id) + strlen(logical_timestamp) + 1];
+    strcat(stpcpy(uniq_id, node_id), logical_timestamp);
+
+    reply_uniq_id = json_object_new_string(uniq_id);
+    json_object_object_add(json_object_object_get(reply, "body"), "id",
+                           reply_uniq_id);
+
+    return reply;
 }
