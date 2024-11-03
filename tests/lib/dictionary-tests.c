5bb8463585352a942246bcffd401af4e4983e86d
Ravi Dayabhai <rdayabhai@anduril.com>
2024-11-03 10:30:04 -0800

    Add dictionary initialization test

--- tests/lib/dictionary-tests.c
+++ tests/lib/dictionary-tests.c
@@ -3,16 +3,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-void setUp(void)
-{
+void setUp(void) {}
 
-    BIG_DICTIONARY = dictionary_init(free);
-    PEERS = malloc(num_peers * sizeof(char*));
-    for (size_t i = 0; i < num_peers; i++)
+void test_instantiate_and_populate_dictionary()
+{
+    const char* keys[] = {"1",  "2",  "3",  "4",  "5",  "6",  "7",  "8",  "9",
+                          "10", "11", "12", "13", "14", "15", "16", "17", "18",
+                          "19", "20", "21", "22", "23", "24", "25"};
+    int values[] = {1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13,
+                    14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
+
+    Dictionary* big_dictionary = dictionary_init(free);
+    size_t num_keys = sizeof(keys) / sizeof(keys[0]);
+
+    for (size_t i = 0; i < num_keys; i++)
     {
-        dictionary_set(big_dictionary, peers[i], channel_state_init());
-        PEERS[i] = strdup(peers[i]);
-        NUM_PEERS++;
+        dictionary_set(big_dictionary, keys[i], &values[i]);
     }
 }
 
@@ -21,6 +27,6 @@ void tearDown(void) {}
 int main(void)
 {
     UNITY_BEGIN();
-    // TODO: Fill in with calls to test functions
+    test_instantiate_and_populate_dictionary();
     return UNITY_END();
 }
